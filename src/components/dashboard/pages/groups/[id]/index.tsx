4292dd9487c4ae2d73f8c638cf8ba154e3757591
Andressa <andressalocatelli@hotmail.com>
2022-11-25 23:14:50 -0300

    solving types conflict

--- src/components/dashboard/pages/groups/[id]/index.tsx
+++ src/components/dashboard/pages/groups/[id]/index.tsx
@@ -1,27 +1,36 @@
 import { Dashboard } from "@/components/dashboard/page";
-import store from "@/store/storeConfig";
+import store, { RootState } from "@/store/storeConfig";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Data from "./data";
+import { IGroupState } from "./store";
 
 const GroupId = () => {
-  const params = useParams();
-  const {id: groupId, title: groupTitle} = params;
-  const {dispatch} = store;
+	const params = useParams();
+	const { id: groupId, title: groupTitle } = params;
+	const { dispatch } = store;
 
-  useEffect(() => {
-    if(typeof groupId != 'undefined') {
-      dispatch(Data.get(groupId));
-    }
-  }, [groupId]);
-  
-  return(
-    <Dashboard hasLeftMenu={true}> 
-      <div className="container" style={{marginTop: '10px'}}>
-        hello
-      </div>
-    </Dashboard>
-  );
+	useEffect(() => {
+		if (typeof groupId != "undefined") {
+			dispatch(Data.get(groupId));
+		}
+	}, [groupId]);
+
+	return (
+		<Dashboard hasLeftMenu={true}>
+			<div className="container" style={{ marginTop: "10px" }}>
+				<ListGroup />
+			</div>
+		</Dashboard>
+	);
 };
 
-export {GroupId};
+const ListGroup = () => {
+	const group = useSelector((state: RootState) => state.group) as IGroupState;
+	console.log(group);
+
+	return <div>hello</div>;
+};
+
+export { GroupId };
