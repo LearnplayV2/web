8767f9ab38cd172dd804ed04372dd013d3e1063d
Andressa <andressalocatelli@hotmail.com>
2022-11-25 23:14:50 -0300

    solving types conflict

--- src/components/dashboard/pages/groups/[id]/data.ts
+++ src/components/dashboard/pages/groups/[id]/data.ts
@@ -9,7 +9,7 @@ class Data {
       dispatch(group.actions.setStatus(FetchStatus.LOADING));
       try {
         const response = await Groups.fetchOne(groupId);
-        dispatch(group.actions.setGroup({ data: response.data }));
+        dispatch(group.actions.setGroup({ data: response.data, status: FetchStatus.SUCCESS }));
       } catch (err) {
         dispatch(group.actions.setStatus(FetchStatus.ERROR));
       }
